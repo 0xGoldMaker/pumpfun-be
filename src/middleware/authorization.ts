111edef9a1256ee38cf63c753b402a0dd6360d69
0xGoldMaker <118068999+0xGoldMaker@users.noreply.github.com>
2024-06-28 01:49:24 -0400

    Update authorization.ts

--- src/middleware/authorization.ts
+++ src/middleware/authorization.ts
@@ -2,6 +2,8 @@ import { NextFunction, Request, Response } from "express"
 import jwt from 'jsonwebtoken'
 import { UserInfo } from "../routes/user";
 
+
+
 export const auth = (req: AuthRequest, res: Response, next: NextFunction): void => {
     // const { authorization } = req.headers;
     // const token = authorization?.split(' ')[1];
